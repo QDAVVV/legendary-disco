ecaa8b5b0cb9c6beaef6b2b53ac09b22ff363535
Louis T <104893531+LouisT1425@users.noreply.github.com>
2024-05-22 12:03:07 +0200

    Ajout de commentaires pour expliquer les commandes

--- gamepad.py
+++ gamepad.py
@@ -9,7 +9,7 @@ def game_controller():
     except Exception:
         print("Could't find a proper controller.")
 
-    gamepad.led = 1
+    gamepad.led = 1 # Ne sert a rien mais faut pas le supprimer
     turning = False
     my_marty = None
     is_connected = False
@@ -17,7 +17,7 @@ def game_controller():
     while True:
         events = inputs.get_gamepad()
         for event in events:
-            if event.code == "BTN_THUMBR" and event.state == 1:
+            if event.code == "BTN_THUMBR" and event.state == 1: # Appuyer sur joystick droit pour se connecter.
                 if not is_connected:
                     print("Trying to connect...")
                     try:
@@ -33,11 +33,12 @@ def game_controller():
             if event.code == "BTN_THUMBL" and event.state == 1:
                 print("BTN_THUMBL pressed.")
 
-            if event.code == "BTN_SOUTH" and event.state == 1:
+            if event.code == "BTN_SOUTH" and event.state == 1: # Bouton B(bouton du bas) = dance
+                print("B button pressed.")
                 if my_marty:
                     my_marty.dance()
 
-            if event.code == "BTN_EAST" and event.state == 1:
+            if event.code == "BTN_EAST" and event.state == 1: #Bouton A(bouton de droite) = read color
                 print("Reading color...")
                 if my_marty:
                     try:
@@ -47,15 +48,16 @@ def game_controller():
                 else:
                     print("Marty not connected !")
 
-            if event.code == "BTN_NORTH" and event.state == 1:
+            if event.code == "BTN_NORTH" and event.state == 1: # Bouton X(bouton du haut) = se met droit
                 print("X button pressed.")
-                my_marty.stand_straight(1000, None)
+                if my_marty:
+                    my_marty.stand_straight(1000, None)
 
             if event.code == "BTN_WEST" and event.state == 1:
                 print("Y button released.")
                 # my_marty.eyes('normal', 1000, None)
 
-            if event.code == "ABS_HAT0Y" and event.state == -1:
+            if event.code == "ABS_HAT0Y" and event.state == -1: # Pad directionnel
                 print("Going forwards.")
                 if my_marty:
                     my_marty.walk(2, 'auto', 0, 35, 1500, None)
@@ -63,22 +65,22 @@ def game_controller():
             if event.code == "ABS_HAT0Y" and event.state == 0:
                 print("Stopped.")
 
-            if event.code == "ABS_HAT0Y" and event.state == 1:
+            if event.code == "ABS_HAT0Y" and event.state == 1: # Pad directionnel
                 print("Going backwards.")
                 if my_marty:
                     my_marty.walk(2, 'auto', 0, -35, 1500, None)
 
-            if event.code == "ABS_HAT0X" and event.state == 1:
+            if event.code == "ABS_HAT0X" and event.state == 1: # Pad directionnel
                 print("Going right.")
                 if my_marty:
                     my_marty.sidestep('right', 2, 35, 1000, None)
 
-            if event.code == "ABS_HAT0X" and event.state == -1:
+            if event.code == "ABS_HAT0X" and event.state == -1: # Pad directionnel
                 print("Going left.")
                 if my_marty:
                     my_marty.sidestep('left', 2, 35, 1000, None)
 
-            if event.code == "BTN_TR" and event.state == 1 and not turning:
+            if event.code == "BTN_TR" and event.state == 1 and not turning: # Petite gachette droite = rotation a droite
                 print("Turning right...")
                 if my_marty:
                     my_marty.walk(2, 'auto', -15, 25, 1500, None)
@@ -88,7 +90,7 @@ def game_controller():
                 print("Done turning.")
                 turning = False
 
-            if event.code == "BTN_TL" and event.state == 1 and not turning:
+            if event.code == "BTN_TL" and event.state == 1 and not turning: # Petite gachette gauche = rotation a gauche
                 print("Turning left...")
                 if my_marty:
                     my_marty.walk(2, 'auto', 15, 25, 1500, None)
