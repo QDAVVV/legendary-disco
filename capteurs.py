2fb9ca2f775a014c9ab50995f03f5e1ce6b50159
Kortans <kortansyolo@gmail.com>
2024-05-28 15:45:57 +0200

    correction des prints, ajout de commentaires

--- capteurs.py
+++ capteurs.py
@@ -4,12 +4,16 @@ def hello():
 
 
 def printcapteurs(my_marty):
-    print("\nCapteur obstacle gauche : "+my_marty.get_obstacle_sensor_reading('left'))
-    print("\nCapteur obstacle droite : "+my_marty.get_obstacle_sensor_reading('right'))
+    print("\nCapteur obstacle gauche : ",my_marty.get_obstacle_sensor_reading('left'))
+    #ne capte des valeurs supérieur à 0 que à partir de genre 2cm
+
+    print("\nCapteur obstacle droite : ",my_marty.get_obstacle_sensor_reading('right'))
+    #ne capte des valeurs supérieur à 1 que à partir de genre 5cm
+
 
     #print("\nCapteur distance test : "+my_marty.get_distance_sensor())
     #Apparement le robot n'est pas doté d'un capteur de distance
 
-    print("\nCapteur couleur : "+my_marty.get_ground_sensor_reading('left'))
+    print("\nCapteur couleur : ",my_marty.get_ground_sensor_reading('left'))
 
-    print("\nBatterie restante : "+my_marty.get_battery_remaining())
+    print("\nBatterie restante : ",my_marty.get_battery_remaining())
