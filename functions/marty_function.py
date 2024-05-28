e8a494807fa50393435a8846755b2e0ff8b2134c
JinxYTW <tyohann974@gmail.com>
2024-05-28 14:02:29 +0200

    Correctif

--- functions/marty_function.py
+++ functions/marty_function.py
@@ -1,3 +1,4 @@
+
 from martypy import Marty
 
 class MartyFunction:
@@ -5,23 +6,32 @@ class MartyFunction:
         self.marty_ip = marty_ip
         self.my_marty = None
 
-    def dance(self):
-        """Make Marty dance."""
-        self.my_marty.dance()
-
     def connect(self):
         """Connect to Marty"""
         self.my_marty = Marty("wifi", self.marty_ip)
         self.my_marty.stand_straight(1000, None)
 
+    def dance(self):
+        """Make Marty dance."""
+        if self.my_marty:
+            self.my_marty.dance()
+        else:
+            print("Marty is not connected")
+
     def stand_straight(self):
         """Make Marty stand straight."""
-        self.my_marty.stand_straight(1000, None)
+        if self.my_marty:
+            self.my_marty.stand_straight(1000, None)
+        else:
+            print("Marty is not connected")
 
     def read_color(self):
         """Read color from Marty's obstacle sensor."""
         try:
-            return self.my_marty.get_obstacle_sensor_reading('left')
+            if self.my_marty:
+                return self.my_marty.get_obstacle_sensor_reading('left')
+            else:
+                print("Marty is not connected")
         except Exception as e:
             print(f"Error reading color: {e}")
             return None
@@ -35,11 +45,17 @@ class MartyFunction:
 
     def sidestep(self, direction='right', steps=2, step_length=35, step_time=1000):
         """Make Marty sidestep."""
-        self.my_marty.sidestep(direction, steps, step_length, step_time, None)
+        if self.my_marty:
+            self.my_marty.sidestep(direction, steps, step_length, step_time, None)
+        else:
+            print("Marty is not connected")
 
     def turn(self, direction='right', steps=2, turn_amount=25, step_time=1500):
         """Make Marty turn."""
-        if direction == 'right':
-            self.my_marty.walk(steps, 'auto', -15, turn_amount, step_time, None)
+        if self.my_marty:
+            if direction == 'right':
+                self.my_marty.walk(steps, 'auto', -15, turn_amount, step_time, None)
+            else:
+                self.my_marty.walk(steps, 'auto', 15, turn_amount, step_time, None)
         else:
-            self.my_marty.walk(steps, 'auto', 15, turn_amount, step_time, None)
+            print("Marty is not connected")
