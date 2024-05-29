13a8a7db8d193a48f904f9bf1858e2e58375a430
Louis T <104893531+LouisT1425@users.noreply.github.com>
2024-05-29 16:15:15 +0200

    Reverted gamepad.py

--- models/gamepad.py
+++ models/gamepad.py
@@ -1,118 +1,105 @@
 from __future__ import print_function
 from martypy import Marty
 import inputs
-from labyrinthe import Labyrinth
 
+from functions.marty_function import MartyFunction
 
-IP = "192.168.0.104"
+class GameController:
+    def __init__(self, marty_ip):
+        self.marty_ip = marty_ip
+        self.marty_functions = None
+        self.turning = False
+        self.is_connected = False
 
+    def run(self):
+        try:
+            gamepad = inputs.devices.gamepads[0]
+        except Exception:
+            print("Couldn't find a proper controller.")
+            return
+        gamepad.led = 1 # Ne sert a rien mais faut pas le supprimer
 
-def game_controller():
-    try:
-        gamepad = inputs.devices.gamepads[0]
-    except Exception:
-        print("Could't find a proper controller.")
+        while True:
+            events = inputs.get_gamepad()
+            for event in events:
+                self.handle_event(event)
 
-    gamepad.led = 1 # Ne sert a rien mais faut pas le supprimer
-    turning = False
-    my_marty = None
-    is_connected = False
+    def connect_to_marty(self):
+        """Attempt to connect to Marty."""
+        if not self.is_connected:
+            print("Trying to connect...")
+            try:
+                self.marty_functions = MartyFunction(self.marty_ip)
+                self.is_connected = True
+                self.marty_functions.connect()
+                print("Connection established!")
+            except Exception as e:
+                print(f"Couldn't connect to Marty: {e}")
 
-    labyrinth = Labyrinth()
+    def handle_event(self, event):
+        if event.code == "BTN_THUMBR" and event.state == 1:  # Connect
+            self.connect_to_marty()
 
-    while True:
-        events = inputs.get_gamepad()
-        for event in events:
-            if event.code == "BTN_THUMBR" and event.state == 1: # Appuyer sur joystick droit pour se connecter.
-                if not is_connected:
-                    print("Trying to connect...")
-                    try:
-                        my_marty = Marty("wifi", IP)
-                        is_connected = True
-                        print("Connection established !")
-                        my_marty.stand_straight(1000, None)
-                    except Exception as e:
-                        print("Couldn't connect to Marty.")
-                else:
-                    print("Already connected !")
+        if event.code == "BTN_THUMBL" and event.state == 1:
+            print("BTN_THUMBL pressed.")
 
-            if event.code == "BTN_THUMBL" and event.state == 1:
-                print("BTN_THUMBL pressed.")
-                labyrinth.auto_walk(my_marty, 'left')
+        if event.code == "BTN_SOUTH" and event.state == 1:  # Dance
+            print("B button pressed.")
+            if self.is_connected:
+                self.marty_functions.dance()
 
-            if event.code == "BTN_SOUTH" and event.state == 1: # Bouton B(bouton du bas) = dance
-                print("B button pressed.")
-                if my_marty:
-                    my_marty.dance()
+        if event.code == "BTN_EAST" and event.state == 1:  # Read color
+            print("Reading color...")
+            if self.is_connected:
+                print(self.marty_functions.read_color())
 
-            if event.code == "BTN_EAST" and event.state == 1: #Bouton A(bouton de droite) = read color
-                print("Reading color...")
-                # Red = 113
-                # Blue = 42
-                # Green = 39
-                # Yellow = 126
-                # Purple = 35
-                if my_marty:
-                    try:
-                        print(my_marty.get_ground_sensor_reading('left'))
-                    except Exception as e:
-                        print(e)
-                else:
-                    print("Marty not connected !")
+        if event.code == "BTN_NORTH" and event.state == 1:  # Stand straight
+            print("X button pressed.")
+            if self.is_connected:
+                self.marty_functions.stand_straight()
 
-            if event.code == "BTN_NORTH" and event.state == 1: # Bouton X(bouton du haut) = se met droit
-                print("X button pressed.")
-                if my_marty:
-                    my_marty.stand_straight(1000, None)
+        if event.code == "BTN_WEST" and event.state == 1:
+            print("Y button released.")
 
-            if event.code == "BTN_WEST" and event.state == 1:
-                print("Y button released.")
-                my_marty.eyes('angry', 1000, None)
-                my_marty.eyes('normal', 1000, None)
+        if event.code == "ABS_HAT0Y" and event.state == -1:  # Walk forward
+            print("Going forwards.")
+            if self.is_connected:
+                self.marty_functions.walk()
 
-            if event.code == "ABS_HAT0Y" and event.state == -1: # Pad directionnel
-                print("Going forwards.")
-                if my_marty:
-                    my_marty.walk(2, 'auto', 0, 35, 1500, None)
+        if event.code == "ABS_HAT0Y" and event.state == 0:
+            print("Stopped.")
 
-            if event.code == "ABS_HAT0Y" and event.state == 0:
-                print("Stopped.")
+        if event.code == "ABS_HAT0Y" and event.state == 1:  # Walk backward
+            print("Going backwards.")
+            if self.is_connected:
+                self.marty_functions.walk(turn=0, step_length=-35)
 
-            if event.code == "ABS_HAT0Y" and event.state == 1: # Pad directionnel
-                print("Going backwards.")
-                if my_marty:
-                    my_marty.walk(2, 'auto', 0, -35, 1500, None)
+        if event.code == "ABS_HAT0X" and event.state == 1:  # Sidestep right
+            print("Going right.")
+            if self.is_connected:
+                self.marty_functions.sidestep(direction='right')
 
-            if event.code == "ABS_HAT0X" and event.state == 1: # Pad directionnel
-                print("Going right.")
-                if my_marty:
-                    my_marty.sidestep('right', 2, 35, 1000, None)
+        if event.code == "ABS_HAT0X" and event.state == -1:  # Sidestep left
+            print("Going left.")
+            if self.is_connected:
+                self.marty_functions.sidestep(direction='left')
 
-            if event.code == "ABS_HAT0X" and event.state == -1: # Pad directionnel
-                print("Going left.")
-                if my_marty:
-                    my_marty.sidestep('left', 2, 35, 1000, None)
+        if event.code == "BTN_TR" and event.state == 1 and not self.turning:  # Turn right
+            print("Turning right...")
+            if self.is_connected:
+                self.marty_functions.turn(direction='right')
+            self.turning = True
 
-            if event.code == "BTN_TR" and event.state == 1 and not turning: # Petite gachette droite = rotation a droite
-                print("Turning right...")
-                if my_marty:
-                    my_marty.walk(2, 'auto', -15, 25, 1500, None)
-                turning = True
+        if event.code == "BTN_TR" and event.state == 0 and self.turning:
+            print("Done turning.")
+            self.turning = False
 
-            if event.code == "BTN_TR" and event.state == 0 and turning:
-                print("Done turning.")
-                turning = False
+        if event.code == "BTN_TL" and event.state == 1 and not self.turning:  # Turn left
+            print("Turning left...")
+            if self.is_connected:
+                self.marty_functions.turn(direction='left')
+            self.turning = True
 
-            if event.code == "BTN_TL" and event.state == 1 and not turning: # Petite gachette gauche = rotation a gauche
-                print("Turning left...")
-                if my_marty:
-                    my_marty.walk(2, 'auto', 15, 25, 1500, None)
-                turning = True
-
-            if event.code == "BTN_TL" and event.state == 0 and turning:
-                print("Done turning.")
-                turning = False
-
-
-if __name__ == "__main__":
-    game_controller()
+        if event.code == "BTN_TL" and event.state == 0 and self.turning:
+            print("Done turning.")
+            self.turning = False
